94d0ff78fab094916ce9949b00495406625d78af
ashtanmistal <ashtanmistal@gmail.com>
2023-11-20 21:43:06 -0800

    Add boilerplate for data loader
    Signed-off-by: ashtanmistal <ashtanmistal@gmail.com>

--- src/utils/data_helper.py
+++ src/utils/data_helper.py
@@ -107,3 +107,32 @@ def train_test_split(config, points, labels):
 
     return train_points, train_labels, test_points, test_labels
 
+
+def read_las_file(file_path):
+    """
+    Reads a .las file and extracts the necessary point cloud data.
+
+    :param file_path: Path to the .las file to be read.
+    :return: A tuple of numpy arrays (points, colors, labels).
+    """
+
+    # Open the .las file
+    las_data = pylas.read(file_path)
+
+    # Extract xyz coordinates
+    points = np.vstack((las_data.x, las_data.y, las_data.z)).transpose()
+
+    # Extract classification labels
+    labels = las_data.classification
+
+    # Check if color information is present and extract it
+    if hasattr(las_data, 'red') and hasattr(las_data, 'green') and hasattr(las_data, 'blue'):
+        colors = np.vstack((las_data.red, las_data.green, las_data.blue)).transpose()
+    else:
+        colors = np.zeros_like(points)  # If no color info, create a dummy array with zeros
+
+    # Normalize or preprocess the points if needed
+    # This would be based on the preprocessing done in the original S3DISDataLoader
+
+    # Return the extracted data
+    return points, colors, labels
