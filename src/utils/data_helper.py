065d460160f49da1ae579c7dca26581f052c8b96
ashtanmistal <ashtanmistal@gmail.com>
2023-12-14 12:39:26 -0800

    various densification and segmentation script changes
    Signed-off-by: ashtanmistal <ashtanmistal@gmail.com>

--- src/utils/data_helper.py
+++ src/utils/data_helper.py
@@ -1,28 +1,8 @@
 import os
 import os.path as osp
-import pylas
 import numpy as np
-import subprocess
 import pandas as pd
 import torch.utils.data as data
-
-
-def split_las(las):
-    """
-    Split the LiDAR data by classification. If it is either class 0 or 1 ("created but never classified",
-    or "unclassified", then save it separately. The rest of the data should be saved in a separate file.
-    :param las: the input LiDAR dataset
-    :return: (classified_las, unclassified_las) corresponding to the split data
-    """
-    classified_las = pylas.create(point_format_id=las.point_format.id)
-    unclassified_las = pylas.create(point_format_id=las.point_format.id)
-    unclassified_las.points = las.points[np.where((las.classification == 0) | (las.classification == 1))]
-    classified_las.points = las.points[np.where((las.classification != 0) & (las.classification != 1))]
-
-    classified_las.header = las.header
-    unclassified_las.header = las.header
-
-    return classified_las, unclassified_las
 
 
 def convert_to_pkl(las, output_path):
@@ -107,31 +87,3 @@ def train_test_split(config, points, labels):
     return train_points, train_labels, test_points, test_labels
 
 
-def read_las_file(file_path):
-    """
-    Reads a .las file and extracts the necessary point cloud data.
-
-    :param file_path: Path to the .las file to be read.
-    :return: A tuple of numpy arrays (points, colors, labels).
-    """
-
-    # Open the .las file
-    las_data = pylas.read(file_path)
-
-    # Extract xyz coordinates
-    points = np.vstack((las_data.x, las_data.y, las_data.z)).transpose()
-
-    # Extract classification labels
-    labels = las_data.classification
-
-    # Check if color information is present and extract it
-    if hasattr(las_data, 'red') and hasattr(las_data, 'green') and hasattr(las_data, 'blue'):
-        colors = np.vstack((las_data.red, las_data.green, las_data.blue)).transpose()
-    else:
-        colors = np.zeros_like(points)  # If no color info, create a dummy array with zeros
-
-    # Normalize or preprocess the points if needed
-    # This would be based on the preprocessing done in the original S3DISDataLoader
-
-    # Return the extracted data
-    return points, colors, labels
